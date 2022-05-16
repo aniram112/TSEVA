382371f29bcc93e3ca8f90e81ba74fb68d84fd68
aniram112 <masyakmarina@gmail.com>
2022-05-16 18:33:47 +0300

    added swift package

--- SuperColorPicker/Controllers/LowPolyController.swift
+++ SuperColorPicker/Controllers/LowPolyController.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import UIKit
-import LowPoly
+import LowPolyPack
 
 class LowPolyController: UIViewController {
     @IBOutlet weak var okButton: UIButton!
@@ -28,7 +28,8 @@ class LowPolyController: UIViewController {
     
     @IBAction func levelChanged(_ sender: Any) {
         //DispatchQueue.global(qos: .utility).async { [self] in
-            lowPolyfy()
+        imageView.image = Methods.lowPolyfy(sourceImage: sourceImage, ratio: (levelControl.selectedSegmentIndex+1) * 10)
+
         //}
         
     }
@@ -69,7 +70,7 @@ class LowPolyController: UIViewController {
         
         self.present(alert, animated: true, completion: nil)
     }
-
+    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[.originalImage] as? UIImage {
@@ -88,74 +89,9 @@ class LowPolyController: UIViewController {
             
             sourceImage = sourceImage.aspectFittedToHeight(sourceImage.size.height*scale)
         }
-        lowPolyfy()
+        imageView.image = Methods.lowPolyfy(sourceImage: sourceImage, ratio: (levelControl.selectedSegmentIndex+1) * 10)
     }
-    
-    func lowPolyfy(){
-        print("entered")
-        
-        guard let image = sourceImage else {
-            return
-        }
-        guard let imageModel = Image.load(image: image) else {
-            return
-        }
-        defer {
-            imageModel.deallocate()
-        }
-        
-        /* detect edges, get all points of edges*/
-        let points = Sobel().edgePoints(imageModel)
-    
-        /* pick out some points from the point set */
-        var selectedPoints = [Point]()
-        let selectRatio: Int = (levelControl.selectedSegmentIndex+1) * 10
-        let selectCount = points.count / selectRatio
-        for i in 0..<selectCount {
-            let point = points[i * selectRatio]
-            let vertex = Point(x: point.x, y: point.y)
-            selectedPoints.append(vertex)
-        }
-        
-        /* add some randomly generated points into it */
-        let possionPoints = Poisson().discSample(Double(imageModel.width), height: Double(imageModel.height), minDistance: Double(imageModel.width) / Double(selectRatio), newPointsCount: 30)
-        selectedPoints.append(contentsOf: possionPoints)
-        
-        /* output triangles with Delaunay Triangulation  */
-        let triangles = Delaunay().triangulate(selectedPoints)
-    
-        
-        /* draw image */
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: (image.cgImage?.width)!, height: (image.cgImage?.height)!), true, UIScreen.main.scale)
-        defer {
-            UIGraphicsEndImageContext()
-        }
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return
-        }
-        context.setAllowsAntialiasing(false)
-        context.setStrokeColor(UIColor.clear.cgColor)
-        context.setLineWidth(0.5)
-        for (_, triangle) in triangles.enumerated()  {
-            let centroid = triangle.centroid()
-            guard let fillColor = imageModel.getPixelColor(x: Int(centroid.x), y: Int(centroid.y))?.cgColor else {
-                return
-            }
-            context.setFillColor(fillColor)
-            context.setStrokeColor(UIColor.yellow.cgColor)
-            context.setLineWidth(0.5)
-            context.move(to: triangle.p0.cgPoint())
-            context.addLine(to: triangle.p1.cgPoint())
-            context.addLine(to: triangle.p2.cgPoint())
-            context.addLine(to: triangle.p0.cgPoint())
-            context.closePath()
-            context.fillPath()
-        }
-        guard let outputImage = UIGraphicsGetImageFromCurrentImageContext() else {
-            return
-        }
-        imageView.image = outputImage
-    }
+
     
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
         let cgpoint = tapGestureRecognizer.location(in: tapGestureRecognizer.view)
